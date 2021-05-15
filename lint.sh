6b299ff6cc640b5afffae3894e621fe9370e2e23
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-15 13:53:30 -0700

    add linter script

---
+++ lint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+EXIT_CODE=0
+
+while getopts ":e" opt; do
+  case $opt in
+    e)
+      EXIT_CODE=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+cppcheck --enable=all --suppressions-list=suppressions.txt --inline-suppr --inconclusive  --force --error-exitcode=${EXIT_CODE} -q src/
