2053d914089a50e37e50fb12b34b6a3d063e719a
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-15 14:31:09 -0700

    add verbosity flag to lint.sh

--- lint.sh
+++ lint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 EXIT_CODE=0
+declare VERBOSITY='-q'
 
-while getopts ":e" opt; do
+while getopts ":e:t:v" opt; do
   case $opt in
     e)
       EXIT_CODE=1
+      ;;
+    v)
+      VERBOSITY="-v"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -13,4 +17,4 @@ while getopts ":e" opt; do
   esac
 done
 
-cppcheck --enable=all --suppressions-list=suppressions.txt --inline-suppr --inconclusive --force --error-exitcode=${EXIT_CODE} -q src/
+cppcheck --enable=all --suppressions-list=suppressions.txt --inline-suppr --inconclusive --force --error-exitcode=${EXIT_CODE} ${VERBOSITY} src/
