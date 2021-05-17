ff3946a513c5897b41eb005f9b94e08e03da1f1b
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-16 21:18:46 -0700

    update stub test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
+// Copyright 2021 Thomas Kercehval
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE( "sample test case", "[second field]" ) {
-    REQUIRE( 1 == 1 );
+TEST_CASE("sample test case", "[second field]") {
+    REQUIRE(1 == 1);
 }
