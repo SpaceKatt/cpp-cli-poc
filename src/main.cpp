fe67b95c4ff4fbe9a35b05dae310b9203947ed6c
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-17 20:40:31 -0700

    update main to reflect updated library API

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,43 @@ int main(int argc, char** argv) {
     try {
         TCLAP::CmdLine cmd("Command description message");
 
-        std::vector<std::string> operators = Operator::getOperatorNames();
-        TCLAP::ValuesConstraint<std::string> allowedOperators(operators);
-        TCLAP::UnlabeledValueArg<std::string> operatorArg(
-            "operator",
-            "Operator\n"
-            "\tadd\t\tadds two numbers, a + b\n"
-            "\tsubtract\tsubtracts two numbers, a - b\n",
-            true,
+        // std::vector<std::string> operators = Operator::getOperatorNames();
+        TCLAP::ValueArg<std::string> addArg(
+            "a",
             "add",
-            &allowedOperators);
+            "adds numbers together, a + b + ... + n\n",
+            false,
+            "add",
+            "add");
 
-        cmd.add(operatorArg);
+        TCLAP::ValueArg<std::string> subtractArg(
+            "s",
+            "subtract",
+            "subtracts two numbers, a - b - ... - n",
+            false,
+            "subtract",
+            "subtract");
+
+        TCLAP::OneOf xorArgs;
+        xorArgs.add(addArg).add(subtractArg);
+        cmd.add(xorArgs);
 
         cmd.parse(argc, argv);
 
-        std::string opName = operatorArg.getValue();
-        auto operatorType = nameToOperatorTypeMap.find(opName)->second;
-        Operator* op = Operator::create(operatorType);
+        // find active command and process it
+        for (auto& arg : xorArgs) {
+            if (arg->isSet()) {
+                std::string opName  = static_cast<
+                    TCLAP::ValueArg<std::string>*
+                >(arg)->getValue();
+                auto operatorType = nameToOperatorTypeMap.find(opName)->second;
+                Operator* op = Operator::create(operatorType);
 
-        op->compute();
-
-        delete op;
+                op->compute();
+                delete op;
+                break;
+            }
+        }
 
         return 0;
     } catch (TCLAP::ArgException &e) {
